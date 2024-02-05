08e06531d8aea990608678d200c4553517b38316
Jatupon Sukkasem <jatupon@softenstorm.com>
2024-02-05 10:52:22 +0700

    Fixed: route.default is not a function.

--- src/route.ts
+++ src/route.ts
@@ -1,5 +1,10 @@
 // Auto[Generating:V1]--->
 // PLEASE DO NOT MODIFY BECAUSE YOUR CHANGES MAY BE LOST.
 
+const route = (app: any) => {
+}
+
+export default route;
+
 // <--- Auto[Generating:V1]
 // PLEASE DO NOT MODIFY BECAUSE YOUR CHANGES MAY BE LOST.
