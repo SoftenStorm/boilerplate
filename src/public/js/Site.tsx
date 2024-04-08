8e3238fc8ba2a7a51a4a0222c08997eb6571c0ec
Jatupon Sukkasem <jatupon@softenstorm.com>
2024-04-08 17:53:15 +0700

    Add a navigation bar.

--- src/public/js/Site.tsx
+++ src/public/js/Site.tsx
@@ -1,5 +1,36 @@
 // Auto[Generating:V1]--->
 // PLEASE DO NOT MODIFY BECAUSE YOUR CHANGES MAY BE LOST.
 
+import {Project, DeclarationHelper} from './helpers/DeclarationHelper';
+import {HTMLHelper} from './helpers/HTMLHelper';
+import {EventHelper} from './helpers/EventHelper';
+
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+
+const _React = React;
+declare let window: any;
+declare let DataManipulationHelper: any;
+
+let expandingPlaceholders = Array.from(document.querySelectorAll('[internal-fsb-init-class]'));
+for (let expandingPlaceholder of expandingPlaceholders) {
+  let forward = JSON.parse((expandingPlaceholder.getAttribute('internal-fsb-init-forward') || '{}').replace(/'/g, '"'));
+  createRoot(expandingPlaceholder).render(React.createElement(DeclarationHelper.get(expandingPlaceholder.getAttribute('internal-fsb-init-class')), {forward: forward, data: window.data || null}, null));
+  if (expandingPlaceholder.firstElementChild) {
+    expandingPlaceholder.parentNode.insertBefore(expandingPlaceholder.firstElementChild, expandingPlaceholder);
+    expandingPlaceholder.parentNode.removeChild(expandingPlaceholder);
+  }  
+}
+
+window.internalFsbSubmit = (guid: string, notation: string, event, callback: any) => {
+  DataManipulationHelper.request(guid, notation, event, callback);
+}
+
+window.internalFsbOpen = (initClass: string, data: any) => {
+  let container = document.createElement('div');
+  createRoot(container).render(React.createElement(DeclarationHelper.get(initClass), {data: data || window.data}, null));
+  document.getElementsByClassName('internal-fsb-begin')[0].appendChild(container.firstElementChild);
+}
+
 // <--- Auto[Generating:V1]
 // PLEASE DO NOT MODIFY BECAUSE YOUR CHANGES MAY BE LOST.
--- src/route.ts
+++ src/route.ts
@@ -1,7 +1,13 @@
 // Auto[Generating:V1]--->
 // PLEASE DO NOT MODIFY BECAUSE YOUR CHANGES MAY BE LOST.
 
+import * as homeController from './controllers/Home';
+
 const route = (app: any) => {
+ app.get("/", homeController.index);
+ app.post("/", homeController.index);
+ app.put("/", homeController.index);
+ app.delete("/", homeController.index);
 }
 
 export default route;
